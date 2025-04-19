1efcd10e5e60bcea5d0a0f8e5a6dafa9fa62aa82
madiyar <madiaraskar0407@gmail.com>
2025-04-19 17:17:34 +0500

    final version for header and hero section

--- src/landingPage/HeroSection.jsx
+++ src/landingPage/HeroSection.jsx
@@ -5,13 +5,23 @@ import style from "../landingPageStyle/HeroSection.module.css";
 
 function HeroSection() {
     return (
-        <section className={style.container} id="heroSection">
-            <h1 className={style.phraseCTA}>GET READY TO <span className={style.highlight}>ACE NUET</span> WITH</h1>
-            <div className={style.animatedText}>
-                <TypingText words={['MOCK TESTS', 'VIDEOS LESSONS', 'SCORE ANALYTICS', 'STUDY PLANS']} />
+        <section className={style.container}
+                 id="heroSection"
+                 style={{ '--bg-image': `url(${heroSectionPicture})` }}>
+            <div className={style.LeftContainer}>
+                <div>
+                    <h1 className={style.phraseCTA}>GET READY TO <span className={style.highlight}>ACE NUET</span> WITH</h1>
+
+                    <div className={style.animatedText}>
+                        <TypingText words={['MOCK TESTS', 'VIDEOS LESSONS', 'SCORE ANALYTICS', 'STUDY PLANS']} />
+                    </div>
+                </div>
+
+                <button className={style.button}>GET STARTED!</button>
             </div>
-            <button className={style.button}>GET STARTED!</button>
-            <img className={style.heroSectionPic} src={heroSectionPicture} alt="picture of NU" />
+
+            <img className={style.heroSectionPic}
+                 src={heroSectionPicture} alt="picture of NU" />
         </section>
     );
 }
